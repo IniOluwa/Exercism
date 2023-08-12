49ded1bdfa5050fed98e82e0120ef68dc5e29ec5
Ini-Oluwa C. Fageyinbo <inioluwafageyinbo@gmail.com>
2023-08-12 21:18:08 +0100

    Naming COrrections Made.

--- Python/word-count/word_count.py
+++ Python/word-count/word_count.py
@@ -1,2 +1,46 @@
-def count_words(sentence):
-    pass
+import collections
+import re
+
+# # Word Count Algorithm
+# def count_sentence(sentence):
+#     sentence = sentence.decode('utf-8', 'ignore')
+#     sentence = sentence.split()
+#     io = collections.defaultdict(int)
+#     exp = ' '
+#     for word in sentence:
+#         word = word.replace(u'\U0001f596', ' ')
+#         word = word.lower()
+#         exp += " " + word
+#     sentence = re.sub(r'[!&@$%^&:,_.]+', ' ', exp)
+#     sentence = sentence.split()
+#     for word in sentence:
+#         io[word] += 1
+#     return io
+
+# Word Count Algorithm
+def count_sentence(sentence):
+    # sentence = sentence.decode('utf-8', 'ignore')
+    # sentence = sentence.split()
+    # io = collections.defaultdict(int)
+    # exp = ' '
+    # for word in sentence:
+    #     word = word.replace(u'\U0001f596', ' ')
+    #     word = word.lower()
+    #     exp += " " + word
+    # sentence = re.sub(r'[!&@$%^&:,_.]+', ' ', exp)
+    # sentence = sentence.split()
+    # for word in sentence:
+    #     io[word] += 1
+    # return io
+
+    # Convert Sentence To Lower Case & Split
+    sentence = sentence.lower().split()
+
+    # Declare Results Dictionary
+    results = {}
+
+    # Return Results
+    return sentence
+
+# Check Code Results
+print(count_sentence("That's the password: 'PASSWORD 123'!, cried the Special Agent.\nSo I fled"))
