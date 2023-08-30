c2ca06faf743645d636dd95eb9eee1dd6a7dd38a
Ini-Oluwa C. Fageyinbo <inioluwafageyinbo@gmail.com>
2023-08-30 06:35:02 +0100

    Two-Fer Added.

---
+++ Python/two-fer/two_fer.py
@@ -0,0 +1,13 @@
+"""
+Two Fer:
+This function takes a name
+and returns the string
+'One for name, one for me'
+if the name is unavailable,
+the function returns the string
+'One for you, one for me'
+"""
+def two_fer(name='you'):
+
+    # Return Two Fer Sentence Based On Name Variable
+    return f'One for {name}, one for me.'
